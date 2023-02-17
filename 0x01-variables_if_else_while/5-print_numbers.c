048521b86fc6e134a83453dfedb0fbc28013bf8b
ok-ritah <omweritah@gmail.com>
2023-02-17 03:06:30 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints out all single digit numbers
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int ch = 0;
+
+	while (ch < 10)
+	{
+		printf("d\n", ch);
+		ch++;
+	}
+	return (0);
+}
